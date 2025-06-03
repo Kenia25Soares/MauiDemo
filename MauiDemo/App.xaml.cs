b41b23fe4985caec06b9e9bbb1a8bbaab63afd96
Kenia25Soares <keniadefreitasmorais@gmail.com>
2025-06-03 16:43:38 +0100

    Creation of myPage

--- MauiDemo/App.xaml.cs
+++ MauiDemo/App.xaml.cs
@@ -5,11 +5,25 @@
         public App()
         {
             InitializeComponent();
+
+      
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            //return new Window(new MinhaPagina());
+            var minhaPagina = new MinhaPagina();
+
+            var navPage = new NavigationPage(minhaPagina);
+
+            navPage.BarBackgroundColor = Colors.Yellow;
+            navPage.BarTextColor = Colors.Blue;
+
+            return new Window(navPage);
+
+
+
+
         }
     }
 }
