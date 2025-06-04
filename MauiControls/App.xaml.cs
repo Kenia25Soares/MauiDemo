2a68e87c2185dc8a55b6c97bb709baa261e9a559
Kenia25Soares <keniadefreitasmorais@gmail.com>
2025-06-04 02:07:19 +0100

    Presentation Controls

---
+++ MauiControls/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MauiControls.Pages;
+
+namespace MauiControls
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            //return new Window(new AppShell());
+            var pagina = new MauiPresentationControls();
+            var navPage = new NavigationPage(pagina)
+            {
+                BarBackgroundColor = Colors.Blue,
+                BarTextColor = Colors.White
+            };
+            return new Window(navPage);
+
+            //var window = new Window(new MauiPresentationControls());
+
+
+            //return window;
+        }
+    }
+}
